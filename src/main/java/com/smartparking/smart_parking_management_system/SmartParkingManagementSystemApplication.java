51628cc796cfcb4e65556a92d086b7253a6b5df4
Amit <amitsavant43@gmail.com>
2024-08-01 10:49:22 +0530

    First Commit

---
+++ src/main/java/com/smartparking/smart_parking_management_system/SmartParkingManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.smartparking.smart_parking_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingManagementSystemApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
