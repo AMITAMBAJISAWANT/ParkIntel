c28ee9576f0b97176db65309b61d88351777b585
Amit <amitsavant43@gmail.com>
2024-08-03 22:44:12 +0530

    Added the DataLoader Class

---
+++ src/main/java/com/smartparking/smart_parking_management_system/config/DataLoader.java
@@ -0,0 +1,32 @@
+package com.smartparking.smart_parking_management_system.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.smartparking.smart_parking_management_system.model.Role;
+import com.smartparking.smart_parking_management_system.repository.RoleRepository;
+
+@Component
+public class DataLoader implements CommandLineRunner{
+    
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) {
+        if(roleRepository.findByName("User")==null){
+            Role userRole=new Role();
+            userRole.setName("User");
+            roleRepository.save(userRole);
+        }
+        if(roleRepository.findByName("Admin")==null){
+            Role adminRole=new Role();
+            adminRole.setName("Admin");
+            roleRepository.save(adminRole);
+        }
+       
+    }
+    
+    
+}
--- src/main/java/com/smartparking/smart_parking_management_system/model/Role.java
+++ src/main/java/com/smartparking/smart_parking_management_system/model/Role.java
@@ -37,4 +37,6 @@ public class Role {
     public int hashCode() {
         return Objects.hash(name);
     }
+
+   
 }
