6dcba716e34ace458bf2ab6bb74c85a7c195b099
Amit <amitsavant43@gmail.com>
2024-08-12 12:12:14 +0530

    Added the CustomerUserDetails

---
+++ src/main/java/com/smartparking/smart_parking_management_system/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.smartparking.smart_parking_management_system.service;
+
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.smartparking.smart_parking_management_system.model.User;
+import com.smartparking.smart_parking_management_system.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService  {
+      
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user=userRepository.findByUsername(username);
+        user.orElseThrow(()-> new UsernameNotFoundException("User Not Found:"+username));
+        return user.map(CustomUserDetails::new).get();
+
+
+        
+    }
+    
+}
