b94e1d6a852f1736a7855f9c13d056c26cb7f05a
Amit <amitsavant43@gmail.com>
2024-08-14 23:59:08 +0530

    update in the jwtUtil

--- src/main/java/com/smartparking/smart_parking_management_system/util/JwtUtil.java
+++ src/main/java/com/smartparking/smart_parking_management_system/util/JwtUtil.java
@@ -1,9 +1,10 @@
 package com.smartparking.smart_parking_management_system.util;
 
 import java.util.Base64;
-
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
 
 public class JwtUtil  {
     private String secret;
@@ -20,5 +21,12 @@ public class JwtUtil  {
 
         }
     }
+    public String getUsernameFromToken(String token){
+        return token;
+        
+    }
+    
+
+    
     
 }
