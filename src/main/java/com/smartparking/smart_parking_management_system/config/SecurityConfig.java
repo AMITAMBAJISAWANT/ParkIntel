337ed1b155a59566dd9ac40abacf20a9be850931
Amit <amitsavant43@gmail.com>
2024-08-18 17:22:14 +0530

    Updated the SecurityConfig

--- src/main/java/com/smartparking/smart_parking_management_system/config/SecurityConfig.java
+++ src/main/java/com/smartparking/smart_parking_management_system/config/SecurityConfig.java
@@ -1,8 +1,56 @@
 package com.smartparking.smart_parking_management_system.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class SecurityConfig   {
+import com.smartparking.smart_parking_management_system.filter.JwtAuthenticationFilter;
+import com.smartparking.smart_parking_management_system.service.CustomUserDetailsService;
 
-   
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable())
+             .authorizeHttpRequests(authorizeHttpRequests->authorizeHttpRequests
+                                     .requestMatchers("/auth/**").permitAll()
+                                     .anyRequest().authenticated()
+                                     )
+                                     .sessionManagement(sessionManagement->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                    .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
+                                    return http.build();
+        
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new  BCryptPasswordEncoder();
+    }
+
     
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+            new AuthenticationManagerBuilder(http.getSharedObject(ObjectPostProcessor.class));
+
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService)
+            .passwordEncoder(passwordEncoder());
+
+        return authenticationManagerBuilder.build();
+    }
 }
