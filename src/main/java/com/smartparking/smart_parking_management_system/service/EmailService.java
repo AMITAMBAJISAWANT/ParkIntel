eb93dd526ce6ada93a569873e4fa57e436005bed
Amit <amitsavant43@gmail.com>
2024-10-04 11:51:56 +0530

    Added the Email Service Class

---
+++ src/main/java/com/smartparking/smart_parking_management_system/service/EmailService.java
@@ -0,0 +1,37 @@
+package com.smartparking.smart_parking_management_system.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to,String subject,String text){
+        MimeMessage message=mailSender.createMimeMessage();
+        try{
+            MimeMessageHelper helper=new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text,true);
+            mailSender.send(message);
+
+        }
+        catch(MessagingException e){
+           throw new IllegalStateException("Fail to send email",e);
+           
+
+
+        }
+    }
+
+
+    
+}
