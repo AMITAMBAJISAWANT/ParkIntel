a71095dcb1487526d7b69f0a77a63457e3e24ef9
Amit <amitsavant43@gmail.com>
2024-10-07 11:20:22 +0530

    Updated the ExceptionHandler class

--- src/main/java/com/smartparking/smart_parking_management_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/smartparking/smart_parking_management_system/exception/GlobalExceptionHandler.java
@@ -18,11 +18,26 @@ public class GlobalExceptionHandler {
         ErrorResponse error=new ErrorResponse(HttpStatus.NOT_FOUND.value(),ex.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
-
-    // public ResponseEntity<ErrorResponse> handleIllegalState(IllegalStateException ex){
-
+     
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalState(IllegalStateException ex){
+         ErrorResponse error= new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage(), LocalDateTime.now());
+         return new ResponseEntity<>(error,HttpStatus.CONFLICT);
         
-    // }
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex){
+        ErrorResponse error= new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+        ErrorResponse error=new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),"An unexpected error occurred",LocalDateTime.now());
+        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
 
 
